bc49504017b5f1bbab34dd7268e2deeb88ca8ffd
Jarett <allieja@mail.gvsu.edu>
2020-09-30 19:21:45 -0400

    Test 4

--- article_difficulty.c
+++ article_difficulty.c
@@ -12,9 +12,9 @@ int main()
     char input[1024];
 
     //string of file name
-    char fileName[5][50];
+    char fileName[20][50];
     //amt of words
-    int words[5];
+    int words[20];
 
     int max=0;
     int min=0;
@@ -30,34 +30,10 @@ int main()
         scanf("%s", fileName[fileAmt - 1]);
         fileN = fopen(fileName[fileAmt - 1] ,"w");
 
-        char nextCh;
-
-        nextCh = fgetc(fileN);
         
-        while(nextCh != EOF){
-        
-            if(nextCh == ' ' || nextCh == '\n'){
-                wrdAmt++;
-            }
-            nextCh = fgetc(fileN);
-        }
-
-        fclose(fileN);
-
-        words[fileAmt - 1] = wrdAmt;
     }
 
-    int i;   
-    int maxVal = words[0];
-    for (i = 1; i < sizeof(words) / sizeof(words[0]); i++) 
-            if (words[i] > maxVal) 
-                max = i;    
-    i = 0;
-    int minVal = words[0];
-
-    for (i = 1; i < sizeof(words) / sizeof(words[0]); i++) 
-        if (words[i] < minVal) 
-            min = i; 
+    
 
     printf("Most difficult document: %s", fileName[max]);
     printf("Least difficult document: %s", fileName[min]);
