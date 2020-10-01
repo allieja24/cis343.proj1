0e662c0c5db34551de7cf2dcafe05ef69962f471
Jarett <allieja@mail.gvsu.edu>
2020-10-01 03:37:25 -0400

    Works on my end - still rough

--- article_difficulty.c
+++ article_difficulty.c
@@ -1,43 +1,57 @@
 /*Jarett Allie
 September 1, 2020
-Summary */
+Summary **/
 
-#include <stdio.h>  
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-int main()  
+int main(int argc, char **argv)
 {
     int fileAmt = 0;
-    
-    char input[1024];
 
     //string of file name
-    char fileName[20][50];
-    //amt of words
-    int words[20];
+    char *fileName[10];
+        //amt of words
+    int words[10];
 
-    int max=0;
-    int min=0;
+    int max = 0;
+    int min = 0;
+    int wrdAmt;
 
+    int counter = 0;
+    for (int i = 0; i < argc; i++)
+    {
+        counter++;
+    }
 
-    while(fgets(input, sizeof(input), stdin) != NULL){
-        
-        int wrdAmt = 0;
+    if(counter == 2){
+        printf("Most difficult document: %s", argv[1]);
+        printf("\n");
+        printf("Least difficult document: %s", argv[1]);
+        return 0;
+    }
+
+    for (int i = 1; i < counter; i++)
+    {
+        wrdAmt = 0;
         fileAmt++;
 
         FILE *fileN;
 
-        scanf("%s", fileName[fileAmt - 1]);
-        fileN = fopen(fileName[fileAmt - 1] ,"w");
+        fileName[fileAmt - 1] = (char *)malloc(20);
+        fileName[fileAmt - 1] = argv[i];
+
+        fileN = fopen(fileName[fileAmt - 1], "r");
 
         char nextCh;
 
         nextCh = fgetc(fileN);
-        
-        while(nextCh != EOF){
-        
-            if(nextCh == ' ' || nextCh == '\n'){
+
+        while (nextCh != EOF)
+        {
+            if (nextCh == ' ' || nextCh == '\n')
+            {
                 wrdAmt++;
             }
             nextCh = fgetc(fileN);
@@ -46,26 +60,30 @@ int main()
         fclose(fileN);
 
         words[fileAmt - 1] = wrdAmt;
-
     }
 
-    int i;   
+    int i;
     int maxVal = words[0];
-    for (i = 1; i < sizeof(words) / sizeof(words[0]); i++) 
-            if (words[i] > maxVal) 
-                max = i;    
-    i = 0;
+    for (i = 1; i < argc; i++)
+        if (words[i] > maxVal)
+        {
+            maxVal = words[i];
+            max = i;
+        }
+
+    int j;
     int minVal = words[0];
 
-    for (i = 1; i < sizeof(words) / sizeof(words[0]); i++) 
-        if (words[i] < minVal) 
-            min = i; 
-
-    printf("Most difficult document: %s", fileName[max]);
-    printf("\n");
+    for (j = 1; j < argc-1; j++)
+    {
+        if (words[j] < minVal)
+        {
+            minVal = words[j];
+            min = j;
+        }
+    }
+    printf("Most difficult document: %s\n", fileName[max]);
     printf("Least difficult document: %s", fileName[min]);
-    
 
     return 0;
 }
-
